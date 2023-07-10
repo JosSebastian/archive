cea2ec04afcf3c1aaa6ba4eaf1e472291d38ac68
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-07-10 09:26:53 -0500

    layout restructuring

--- src-nuxt/components/bar/side.vue
+++ src-nuxt/components/bar/side.vue
@@ -1,7 +1,18 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>Component: SideBar</div>
+  <div class="w-96 h-full flex flex-col gap-1">
+    <div
+      class="m-0 p-1.5 border-2 rounded-md border-neutral-200 bg-neutral-100"
+    >
+      Component: SideBar
+    </div>
+    <div
+      class="m-0 p-1.5 border-2 rounded-md border-neutral-200 bg-neutral-100 grow"
+    >
+      Component: SideBar
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- src-nuxt/layouts/default.vue
+++ src-nuxt/layouts/default.vue
@@ -4,9 +4,7 @@
   <div
     class="fixed w-screen h-screen m-0 p-1 bg-neutral-100 flex flex-row gap-1"
   >
-    <BarSide
-      class="w-96 h-full m-0 p-1.5 border-2 rounded-md border-neutral-200 bg-neutral-100"
-    />
+    <BarSide />
     <div
       class="w-full h-full m-0 p-1.5 border-2 rounded-md border-neutral-200/50 bg-neutral-100 flex flex-col"
     >
