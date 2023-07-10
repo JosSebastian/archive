941f0d91d71baa1d8dfe5f57b7e8fdbf569bdcbf
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-07-10 12:43:59 -0500

    primitive button implementation

---
+++ src-nuxt/components/primitive/button.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <button
+    class="m-0 px-2 py-1 rounded hover:outline outline-1 outline-neutral-300 dark:outline-neutral-600 bg-neutral-100 dark:bg-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-700 transition flex flex-row gap-1"
+  >
+    <slot />
+  </button>
+</template>
+
+<style scoped></style>
