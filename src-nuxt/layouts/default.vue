4718dc2dc2304830f2d42a9fa9d435e90bc192ed
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-07-11 10:42:23 -0500

    layout restructuring

--- src-nuxt/layouts/default.vue
+++ src-nuxt/layouts/default.vue
@@ -6,10 +6,12 @@
   >
     <BarSide />
     <div
-      class="w-full h-full m-0 p-1.5 rounded-md border border-neutral-300 dark:border-neutral-600 bg-neutral-100 dark:bg-neutral-800 flex flex-col"
+      class="w-full h-full m-0 p-1.5 rounded-md border border-neutral-300 dark:border-neutral-600 bg-neutral-100 dark:bg-neutral-800 flex flex-col justify-between gap-1"
     >
       <BarTop />
-      <slot />
+      <div class="w-full h-full rounded-lg outline outline-0 overflow-auto">
+        <slot />
+      </div>
       <BarBottom />
     </div>
   </div>
--- src-nuxt/pages/index.vue
+++ src-nuxt/pages/index.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="h-full">Page: /Index</div>
+  <div class="flex flex-col">
+    <span class="fixed w-full bg-neutral-100 dark:bg-neutral-800">
+      Page: /Index
+    </span>
+    <div class="mt-9 mb-3"></div>
+  </div>
 </template>
 
 <style scoped></style>
