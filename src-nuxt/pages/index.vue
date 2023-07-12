fd81da732688bf48314f0c32c99376f39075815a
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-07-12 12:59:59 -0500

    layout restructuring

--- src-nuxt/pages/index.vue
+++ src-nuxt/pages/index.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="flex flex-col">
-    <span class="fixed w-full bg-neutral-100 dark:bg-neutral-800">
-      Page: /Index
+  <div class="w-full h-full flex flex-col">
+    <span class="fixed w-full h-fit bg-neutral-100 dark:bg-neutral-800">
+      Page: /
     </span>
     <div class="mt-9 mb-3"></div>
   </div>
