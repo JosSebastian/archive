2411b71bcfa5431ee83ea25c6024834b09c9acee
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-07-11 23:58:54 -0500

    primitive button restructuring

--- src-nuxt/components/primitive/button.vue
+++ src-nuxt/components/primitive/button.vue
@@ -1,8 +1,16 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// __________________ Types __________________ \\
+type PrimitiveButtonProps = {
+  type?: HTMLButtonElement["type"];
+};
+// __________________ Props __________________ \\
+const { type = "button" } = defineProps<PrimitiveButtonProps>();
+</script>
 
 <template>
   <button
-    class="m-0 px-2 py-1 rounded hover:outline outline-1 outline-neutral-300 dark:outline-neutral-600 bg-neutral-100 dark:bg-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-700 transition flex flex-row gap-1"
+    v-bind:type="type"
+    class="m-0 px-2 py-1 rounded hover:outline outline-1 outline-neutral-300 dark:outline-neutral-600 focus:outline-neutral-400 bg-neutral-100 dark:bg-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-700 transition flex flex-row gap-1"
   >
     <slot />
   </button>
