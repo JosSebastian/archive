4f274757d149caef8c1027da4fca007aeed39d40
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-07-12 22:49:41 -0500

    layout refactoring

--- src-nuxt/layouts/default.vue
+++ src-nuxt/layouts/default.vue
@@ -1,18 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const user = useSupabaseUser();
+</script>
 
 <template>
   <div
     class="fixed w-screen h-screen m-0 p-1 bg-neutral-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100 flex flex-row gap-1"
   >
-    <BarSide />
+    <BarSide v-if="user" />
     <div
       class="w-full h-full m-0 p-1.5 rounded-md border border-neutral-300 dark:border-neutral-600 bg-neutral-100 dark:bg-neutral-800 flex flex-col justify-between gap-1"
     >
-      <BarTop />
+      <BarTop v-if="user" />
       <div class="w-full h-full rounded-lg outline outline-0 overflow-auto">
         <slot />
       </div>
-      <BarBottom />
+      <BarBottom v-if="user" />
     </div>
   </div>
 </template>
--- src-nuxt/pages/index.vue
+++ src-nuxt/pages/index.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="w-full h-full flex flex-col">
-    <span class="fixed w-full h-fit bg-neutral-100 dark:bg-neutral-800">
+    <span class="w-full h-fit bg-neutral-100 dark:bg-neutral-800">
       Page: /
     </span>
     <div class="mt-9 mb-3"></div>
