6a3a943d90d6e679f00d4048ec6a273134bb788a
JosSebastian <98227328+JosSebastian@users.noreply.github.com>
2023-07-14 11:56:12 -0500

    primitives restructuring

--- src-nuxt/pages/archive/create.vue
+++ src-nuxt/pages/archive/create.vue
@@ -1,18 +1,36 @@
 <script lang="ts" setup>
-const content = ref({
-  title: "Create",
-  description: "Create a new archive.",
-  icon: "operator/add",
-  path: "/archive/create",
+import { Content } from "../../../types";
+const content = ref<Content>({
+  id: 0,
+
+  medium: "",
+  title: "",
+  description: "",
+  genre: [],
+  status: "",
+  rating: 0,
+
+  volume: 0,
+  chapter: 0,
+  volumes: [],
+
+  note: "",
+  notes: [],
+
+  time: {
+    first: "",
+    last: "",
+  },
 });
 </script>
 
 <template>
   <div class="w-full h-full m-0 p-1 flex flex-col">
     <span class="w-96 h-fit"> Page: /Archive/Create </span>
-    <div class="overflow-auto">
-      {{ content }}
-    </div>
+    <div
+      class="overflow-auto h-full flex flex-col justify-center items-center"
+    ></div>
+    {{ content }}
   </div>
 </template>
 
